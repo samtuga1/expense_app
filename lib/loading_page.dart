3db4846c1d215c6cd3c794dffa646d55465e539a
samtuga1 <79772304+samtuga1@users.noreply.github.com>
2021-12-12 15:00:35 +0000

    Initial commit

---
+++ lib/loading_page.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class LoadingPage extends StatefulWidget {
+  const LoadingPage({Key? key}) : super(key: key);
+
+  @override
+  _LoadingPageState createState() => _LoadingPageState();
+}
+
+class _LoadingPageState extends State<LoadingPage> {
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold();
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+import 'loading_page.dart';
+
+void main() {
+  runApp(const MaterialApp(
+    home: LoadingPage(),
+  ));
+}
